713cfb86e8b0b42ba3b6c56ad4b6383b5e27d1d0
Darbastoks <gaidys.993@gmail.com>
2026-03-23 21:32:51 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-            console.log(`${req.method} ${req.url} - ${ms}ms`);
+const express = require('express');
+const cors = require('cors');
+const compression = require('compression');
+const db = require('./database');
+const { exec } = require('child_process');
+const util = require('util');
+const path = require('path');
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
+const execPromise = util.promisify(exec);
+
+const app = express();
+
+// 
